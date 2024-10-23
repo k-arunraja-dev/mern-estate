51b09e50174d69ba434ea7f20ec6a97c6f9bc524
Your Name <your_email@example.com>
2024-10-23 12:51:29 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.contoller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.contoller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
-
+router.post('/update/:id', verifyToken, updateListing);
 export default router;
