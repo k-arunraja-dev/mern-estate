b975b881fb26a58fae4ee16a2cb275438c55e146
Your Name <your_email@example.com>
2024-10-25 12:00:51 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Raja Estate</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Raja Estate, your trusted partner in finding the perfect property to call home. Our platform is designed to offer an easy, efficient, and engaging way for users to explore a range of real estate options. Whether you’re looking for a new place to rent or a property investment opportunity, we’re here to connect you with properties that meet your needs. Raja Estate offers a vast selection of listings that can be tailored to your specific preferences. Our commitment to innovation and customer experience ensures that finding your ideal space is a seamless journey.</p>
+      <p className='mb-4 text-slate-700'>At the heart of Raja Estate is an advanced property listing system that brings together the latest offers, rentals, and sales from across the market. We utilize up-to-date listings and engaging visuals to help you experience each property virtually, making it easier to make informed choices. Our user-friendly design incorporates features like the “Recent Offers” section, where you can discover the latest deals on the market, and the Swiper slider, which highlights curated listings for a dynamic browsing experience. We strive to present all the information you need so that your search is both enjoyable and efficient.</p>
+      <p className='mb-4 text-slate-700'>Raja Estate is built with you in mind, incorporating powerful search filters, quick navigation, and an intuitive design that enhances the entire property-finding process. We believe that finding your perfect place should be as enjoyable as living in it, and our mission is to simplify and enhance your real estate journey. With Raja Estate, your next home or investment is just a few clicks away—explore, discover, and find the place that best suits your lifestyle.</p>
+    </div>
   )
 }
