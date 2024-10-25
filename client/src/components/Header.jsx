0b76d3ae6214d7df1bba497099bbc66e358479f8
Your Name <your_email@example.com>
2024-10-25 11:47:16 +0530

    complete home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -46,12 +46,12 @@ export default function Header() {
                 </form>
                 <ul className='flex gap-4'>
                     <Link to='/'>
-                        <li className='hidden sm:inline text-slate-700'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
                             Home
                         </li>
                     </Link>
                     <Link to='/about'>    
-                        <li className='hidden sm:inline text-slate-700'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
                             About
                         </li>
                     </Link>
